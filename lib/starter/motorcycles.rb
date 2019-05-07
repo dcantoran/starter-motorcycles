2f4856b2d886abbcc69eab7801a73ba8173cb369
David Cantoran <davidcantoran36@gmail.com>
2019-05-07 15:26:34 +0000

    first commit

---
+++ lib/starter/motorcycles.rb
@@ -0,0 +1,8 @@
+require "starter/motorcycles/version"
+
+module Starter
+  module Motorcycles
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/starter/motorcycles/version.rb
@@ -0,0 +1,5 @@
+module Starter
+  module Motorcycles
+    VERSION = "0.1.0"
+  end
+end
